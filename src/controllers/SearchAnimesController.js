a5d00a6c0cdbc993d47f818ef07ccbef8e3930bc
zBrownie <bru.nomoraes@hotmail.com>
2020-03-10 22:46:46 -0300

    update teste

--- src/controllers/SearchAnimesController.js
+++ src/controllers/SearchAnimesController.js
@@ -1,45 +1,49 @@
-const Anime = require('../models/Anime')
-
+const Anime = require("../models/Anime");
 
 module.exports = {
-    async index(request, response) {
-        const { id } = request.id;
-        let anime = await Anime.findOne({ id })
+  async index(request, response) {
+    const { id } = request.id;
+    let anime = await Anime.findOne({ id });
 
-        if (anime) {
-            return response.json(anime)
-        }
-
-        return response.json({
-            "error": "Anime not find"
-        })
-    },
-    async dayanimes(request, response) {
-
-        let anime = await Anime.find({ streaming: true, pos: new Date().getDay() })
-
-        if (anime) {
-            return response.json(anime)
-        }
-
-        return response.json({
-            "error": "Anime not find"
-        })
-
-    },
-    async seasonanimes(request, response) {
-
-        let anime = await Anime.find({ streaming: true })
-
-        if (anime) {
-            return response.json(anime)
-        }
-
-        return response.json({
-            "error": "Anime not find"
-        })
-
+    if (anime) {
+      return response.json(anime);
     }
 
+    return response.json({
+      error: "Anime not find"
+    });
+  },
+  async dayanimes(request, response) {
+    let anime = await Anime.find({ streaming: true, pos: new Date().getDay() });
 
-}
+    if (anime) {
+      return response.json(anime);
+    }
+
+    return response.json({
+      error: "Anime not find"
+    });
+  },
+  async seasonanimes(request, response) {
+    let anime = await Anime.find({ streaming: true });
+
+    if (anime) {
+      return response.json(anime);
+    }
+
+    return response.json({
+      error: "Anime not find"
+    });
+  },
+  async seasongetter(request, response) {
+    let anime = await Anime.find({ streaming: true }).sort({ pos: -1 });
+
+    if (anime) {
+      return response.json(anime);
+    }
+
+    return response.json({
+      error: "Anime not find"
+    });
+  }
+};
